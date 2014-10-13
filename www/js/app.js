e1f8431b4588964695f2ea6e2570fa1f1c3780d3
Shi Kai <khiewshikai@Khiews-MacBook-Pro.local>
2014-10-13 10:52:22 +0800

    added home, add event, add friend

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.services', 'starter.controllers', 'angular-datepicker'])
 
 
 .config(function($stateProvider, $urlRouterProvider) {
@@ -17,53 +17,66 @@ angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
   $stateProvider
 
     // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: '/tab',
-      abstract: true,
-      templateUrl: 'templates/tabs.html'
-    })
-
-    // the pet tab has its own child nav-view and history
-    .state('tab.home', {
+    .state('home', {
       url: '/home',
-      views: {
-        'pets-tab': {
-          templateUrl: 'templates/pet-index.html',
-          controller: 'PetIndexCtrl'
-        }
-      }
+      templateUrl: 'templates/home.html',
+      controller: 'PetIndexCtrl'
+    })
+    
+    .state('addfriend', {
+      url: '/addfriend',
+      templateUrl: 'templates/addfriend.html',
+      controller: 'PetIndexCtrl'
+    })
+    
+    .state('addevent', {
+      url: '/addevent',
+      templateUrl: 'templates/addevent.html',
+      controller: 'PetIndexCtrl'
     })
 
-    .state('tab.pet-detail', {
-      url: '/pet/:petId',
-      views: {
-        'pets-tab': {
-          templateUrl: 'templates/pet-detail.html',
-          controller: 'PetDetailCtrl'
-        }
-      }
-    })
 
-    .state('tab.adopt', {
-      url: '/adopt',
-      views: {
-        'adopt-tab': {
-          templateUrl: 'templates/adopt.html'
-        }
-      }
-    })
-
-    .state('tab.about', {
-      url: '/about',
-      views: {
-        'about-tab': {
-          templateUrl: 'templates/about.html'
-        }
-      }
-    });
+//    // the pet tab has its own child nav-view and history
+//    .state('tab.home', {
+//      url: '/home',
+//      views: {
+//        'pets-tab': {
+//          templateUrl: 'templates/home.html',
+//          controller: 'PetIndexCtrl'
+//        }
+//      }
+//    })
+//
+//    .state('tab.addfriend', {
+//      url: '/addfriend',
+//      views: {
+//        'pets-tab': {
+//          templateUrl: 'templates/addfriend.html',
+//          controller: 'PetDetailCtrl'
+//        }
+//      }
+//    })
+//
+//    .state('tab.adopt', {
+//      url: '/adopt',
+//      views: {
+//        'adopt-tab': {
+//          templateUrl: 'templates/adopt.html'
+//        }
+//      }
+//    })
+//
+//    .state('tab.about', {
+//      url: '/about',
+//      views: {
+//        'about-tab': {
+//          templateUrl: 'templates/about.html'
+//        }
+//      }
+//    });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/home');
+  $urlRouterProvider.otherwise('/home');
 
 });
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,18 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['angular-datepicker'])
 
 
 // A simple controller that fetches a list of data from a service
-.controller('PetIndexCtrl', function($scope, PetService) {
+.controller('PetIndexCtrl', function($scope, PetService, $ionicModal) {
   // "Pets" is a service returning mock data (services.js)
   $scope.pets = PetService.all();
+  $ionicModal.fromTemplateUrl('modal.html', function($ionicModal) {
+        $scope.modal = $ionicModal;
+    }, {
+        // Use our scope for the scope of the modal to keep it simple
+        scope: $scope,
+        // The animation we want to use for the modal entrance
+        animation: 'slide-in-up'
+    });  
 })
 
 
