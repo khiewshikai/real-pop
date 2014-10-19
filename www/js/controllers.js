b1a2463e44472204a5cfa8fb3cbf78c9a25f53a1
Shi Kai <khiewshikai@Khiews-MacBook-Pro.local>
2014-10-19 23:16:05 +0800

    updated add event

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,14 +5,30 @@ angular.module('starter.controllers', ['angular-datepicker'])
 .controller('PetIndexCtrl', function($scope, PetService, $ionicModal) {
   // "Pets" is a service returning mock data (services.js)
   $scope.pets = PetService.all();
-  $ionicModal.fromTemplateUrl('modal.html', function($ionicModal) {
-        $scope.modal = $ionicModal;
-    }, {
-        // Use our scope for the scope of the modal to keep it simple
-        scope: $scope,
-        // The animation we want to use for the modal entrance
-        animation: 'slide-in-up'
-    });  
+    $ionicModal.fromTemplateUrl('add-members.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+      // Execute action
+    });
 })
 
 
